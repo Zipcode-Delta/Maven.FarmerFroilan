b14e16f604ce8399de868f1dc62f1aec4d7bac12
Nathan <natefintak@gmail.com>
2025-03-30 12:32:00 -0400

    start of Farm class

--- src/main/java/com/zipcodewilmington/froilansfarm/Farm.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm.java
@@ -1,4 +1,16 @@
 package com.zipcodewilmington.froilansfarm;
 
 public class Farm {
+    private FarmHouse farmhouse;
+    private Stable stable1 = new Stable();
+    Stable stable2 = new Stable();
+    Stable stable3 = new Stable();
+    ChickenCoop coop1 = new ChickenCoop();
+    ChickenCoop coop2 = new ChickenCoop();
+    ChickenCoop coop3 = new ChickenCoop();
+    ChickenCoop coop4 = new ChickenCoop();
+
+    Farm() {
+        //this.farmHouse = new FarmHouse();
+    }
 }
