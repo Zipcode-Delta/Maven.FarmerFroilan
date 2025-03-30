42e796c995188052e6a4c0b72c9610a35089fea4
kanika-sharma-pro <kanikasharma1908@gmail.com>
2025-03-30 10:59:15 -0400

    created 1 test for CropRow and created CropRow

--- src/main/java/com/zipcodewilmington/froilansfarm/CropRow.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/CropRow.java
@@ -1,4 +1,20 @@
 package com.zipcodewilmington.froilansfarm;
 
 public class CropRow {
+    private int numOfCrops;
+    public CropRow() {
+        this.numOfCrops = 0;
+    }
+
+    public int getNumOfCrops() {
+        return numOfCrops;
+    }
+
+    public void setNumOfCrops(int numOfCrops) {
+        this.numOfCrops = numOfCrops;
+    }
+
+    public void addCrops(int crops) {
+        this.numOfCrops += crops;
+    }
 }
--- src/test/java/com/zipcodewilmington/froilansfarm/CropRowTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/CropRowTest.java
@@ -1,5 +1,27 @@
 package com.zipcodewilmington.froilansfarm;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
 class CropRowTest {
+private CropRow cropRow;
+@BeforeEach
+void setUp() {
+    cropRow = new CropRow();
+}
+@Test
+    void testSetNumOfCrops() {
+    cropRow.setNumOfCrops(10);
+    assertEquals(10,cropRow.getNumOfCrops());
+}
+
+
+
+
+
+
+
 
 }
