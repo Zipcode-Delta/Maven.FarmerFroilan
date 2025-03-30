f783c4a988d59e8ac581ab3abff5b91c9bff44e3
Nathan <natefintak@gmail.com>
2025-03-30 13:07:48 -0400

    adds some instance vars to Farm class

--- src/main/java/com/zipcodewilmington/froilansfarm/Farm.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm.java
@@ -1,21 +1,46 @@
 package com.zipcodewilmington.froilansfarm;
-import com.zipcodewilmington.froilansfarm.FarmHouse;
 
 public class Farm {
-    private final FarmHouse farmHouse;
-    private final Stable stable1;
-    Stable stable2 = new Stable();
-    Stable stable3 = new Stable();
-    ChickenCoop coop1 = new ChickenCoop();
-    ChickenCoop coop2 = new ChickenCoop();
-    ChickenCoop coop3 = new ChickenCoop();
-    ChickenCoop coop4 = new ChickenCoop();
+    FarmHouse farmHouse;
+    Stable stable1;
+    Stable stable2;
+    Stable stable3;
+    ChickenCoop coop1;
+    ChickenCoop coop2;
+    ChickenCoop coop3;
+    ChickenCoop coop4;
+    Field field;
 
     Farm() {
         this.farmHouse = new FarmHouse();
         this.stable1 = new Stable();
         this.stable2 = new Stable();
         this.stable3 = new Stable();
-
+        this.coop1 = new ChickenCoop();
+        this.coop2 = new ChickenCoop();
+        this.coop3 = new ChickenCoop();
+        this.coop4 = new ChickenCoop();
+        this.field = new Field();
     }
+
+    /*
+    will need methods for:
+    sundayRoutine();  schedule given
+    mondayRoutine();  schedule given
+    tuesdayRoutine();  schedule given
+    wednesdayRoutine();
+    thursdayRoutine();
+    fridayRoutine();
+    saturdayRoutine();
+    morningRoutine();  used every morning, regardless of day
+    feedHorse();  part of morning routine
+    rideHorse();  part of morning routine
+    eatBreakfast();  part of morning routine
+
+    should we have a method that sets up the farm,
+    creates the Person, Horse, and Chicken objects and adds them to the shelters?
+    are these returning void? should feedHorse() return the new amount of EarCorn left?
+    can eatBreakfast() be a boolean that returns true after it's completed?
+    can rideHorse() also be a boolean to confirm that each horse is rode each day?
+     */
 }
