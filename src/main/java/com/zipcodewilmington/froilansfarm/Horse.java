6d2d84f7736f71b38155cc03830987133dbf28a1
kanika-sharma-pro <kanikasharma1908@gmail.com>
2025-03-30 20:59:05 -0400

    added 3 test for horse

--- src/main/java/com/zipcodewilmington/froilansfarm/Horse.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Horse.java
@@ -1,13 +1,33 @@
 package com.zipcodewilmington.froilansfarm;
 
-public class Horse extends Animal implements Rideable{
+public class Horse extends Animal implements Rideable, NoiseMaker{
+    private boolean beingRidden;
+    //private List<Edible> foodEaten;
+
+    public Horse() {
+        this.beingRidden = false;
+        //this.foodEaten = new ArrayList<>();
+    }
+
+
     @Override
     public String makeNoise() {
-        return "";
+        return "Neigh";
     }
 
     @Override
     public boolean ride() {
+        this.beingRidden = true;
         return false;
     }
+public void dismount() {
+        this.beingRidden = false;
+}
+public boolean isBeingRidden() {
+        return beingRidden;
+}
+   // @Override
+    //    public void eat("Edible Food") {
+    //        System.out.println("Horse is eating");
+    //    }
 }
--- src/test/java/com/zipcodewilmington/froilansfarm/HorseTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/HorseTest.java
@@ -1,7 +1,34 @@
 package com.zipcodewilmington.froilansfarm;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HorseTest {
+    @Test
+    void testMakeNoise() {
+        Horse horse = new Horse();
+
+        assertEquals("Neigh", horse.makeNoise(), "Horse should neigh");
+    }
+
+    @Test
+    void testRide() {
+        Horse horse = new Horse();
+
+        assertFalse(horse.isBeingRidden());
+        horse.ride();
+        assertTrue(horse.isBeingRidden());
+
+    }
+
+    @Test
+    void testDismountHorse() {
+        Horse horse = new Horse();
+        horse.ride();
+        assertTrue(horse.isBeingRidden());
+        horse.dismount();
+        assertFalse(horse.isBeingRidden());
+    }
 
 }
