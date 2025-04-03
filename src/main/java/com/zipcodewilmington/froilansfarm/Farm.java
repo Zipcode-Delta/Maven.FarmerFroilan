edadc842c99f1fc094dea1a70bb04c76cca66ee0
Nathan <natefintak@gmail.com>
2025-04-03 13:48:30 -0400

    adds animal objects to shelters in Farm

--- src/main/java/com/zipcodewilmington/froilansfarm/Farm.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm.java
@@ -18,6 +18,26 @@ public class Farm {
         froilanda.setName("Froilanda");
         farmHouse.add(froilan);
         farmHouse.add(froilanda);
+        Horse horse1 = new Horse(); Horse horse2 = new Horse();
+        Horse horse3 = new Horse(); Horse horse4 = new Horse();
+        Horse horse5 = new Horse(); Horse horse6 = new Horse();
+        Horse horse7 = new Horse(); Horse horse8 = new Horse();
+        Horse horse9 = new Horse(); Horse horse10 = new Horse();
+        stable1.add(horse1); stable1.add(horse2); stable1.add(horse3); stable1.add(horse4);
+        stable2.add(horse5); stable2.add(horse6); stable2.add(horse7); stable2.add(horse8);
+        stable3.add(horse9); stable3.add(horse10);
+        Chicken chicken1 = new Chicken(); Chicken chicken2 = new Chicken();
+        Chicken chicken3 = new Chicken(); Chicken chicken4 = new Chicken();
+        Chicken chicken5 = new Chicken(); Chicken chicken6 = new Chicken();
+        Chicken chicken7 = new Chicken(); Chicken chicken8 = new Chicken();
+        Chicken chicken9 = new Chicken(); Chicken chicken10 = new Chicken();
+        Chicken chicken11 = new Chicken(); Chicken chicken12 = new Chicken();
+        Chicken chicken13 = new Chicken(); Chicken chicken14 = new Chicken();
+        Chicken chicken15 = new Chicken();
+        coop1.add(chicken1); coop1.add(chicken2); coop1.add(chicken3); coop1.add(chicken4);
+        coop2.add(chicken5); coop2.add(chicken6); coop2.add(chicken7); coop2.add(chicken8);
+        coop3.add(chicken9); coop3.add(chicken10); coop3.add(chicken11); coop3.add(chicken12);
+        coop4.add(chicken13); coop4.add(chicken14); coop4.add(chicken15);
     }
 
     void runFarm() {
