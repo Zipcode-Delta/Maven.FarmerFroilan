d799fcb449cc432b5208ccef772056c7dc505b03
Nathan <natefintak@gmail.com>
2025-04-03 17:58:50 -0400

    adds new constructor to Horse class

--- src/main/java/com/zipcodewilmington/froilansfarm/Farm.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm.java
@@ -12,11 +12,11 @@ public class Farm {
     Field field = new Field();
     Farmer froilan = new Farmer();
     Pilot froilanda = new Pilot();
-    Horse horse1 = new Horse(); Horse horse2 = new Horse();
-    Horse horse3 = new Horse(); Horse horse4 = new Horse();
-    Horse horse5 = new Horse(); Horse horse6 = new Horse();
-    Horse horse7 = new Horse(); Horse horse8 = new Horse();
-    Horse horse9 = new Horse(); Horse horse10 = new Horse();
+    Horse horse1 = new Horse("horse1"); Horse horse2 = new Horse("horse2");
+    Horse horse3 = new Horse("horse3"); Horse horse4 = new Horse("horse4");
+    Horse horse5 = new Horse("horse5"); Horse horse6 = new Horse("horse6");
+    Horse horse7 = new Horse("horse7"); Horse horse8 = new Horse("horse8");
+    Horse horse9 = new Horse("horse9"); Horse horse10 = new Horse("horse10");
     Chicken chicken1 = new Chicken(); Chicken chicken2 = new Chicken();
     Chicken chicken3 = new Chicken(); Chicken chicken4 = new Chicken();
     Chicken chicken5 = new Chicken(); Chicken chicken6 = new Chicken();
