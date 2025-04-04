409cf96a0e4e2d69effee01551ae79d0851f10d1
Nathan <natefintak@gmail.com>
2025-04-04 10:39:05 -0400

    adds tests for YamPlant class

--- src/test/java/com/zipcodewilmington/froilansfarm/YamPlantTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/YamPlantTest.java
@@ -5,15 +5,22 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.Assert;
 import org.junit.Test;
 
-class YamPlantTest {
+public class YamPlantTest {
 
     @Test
-    public void getNumberOfEdibleCropsTest() {
+    public void setNumberOfEdibleCropsTest() {
         Crop yamPlant = new YamPlant("yamPlant");
         yamPlant.setNumberOfEdibleCrops(30);
         Assert.assertEquals(30, yamPlant.getNumberOfEdibleCrops());
     }
 
-
+    @Test
+    public void addEdibleCropsTest() {
+        Crop yamPlant = new YamPlant("yamPlant");
+        yamPlant.setNumberOfEdibleCrops(30);
+        Assert.assertEquals(30, yamPlant.getNumberOfEdibleCrops());
+        yamPlant.addEdibleCrops(20);
+        Assert.assertEquals(50, yamPlant.getNumberOfEdibleCrops());
+    }
 
 }
